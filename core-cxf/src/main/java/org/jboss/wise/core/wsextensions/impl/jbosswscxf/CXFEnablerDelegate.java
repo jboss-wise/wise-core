c34586c307ebf585664c9e1bc8e89681b1b94c92
alessio.soldano@jboss.com <alessio.soldano@jboss.com@a17ab928-1b57-0410-8599-cedcd8a065c6>
2010-12-20 14:07:41 +0000

    [WISE-169] Adding WS-Addressing support with CXF stack; adding new integration testsuite module for integration tests with cxf stack

---
+++ core-cxf/src/main/java/org/jboss/wise/core/wsextensions/impl/jbosswscxf/CXFEnablerDelegate.java
@@ -0,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source Copyright 2006, JBoss Inc., and
+ * individual contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.jboss.wise.core.wsextensions.impl.jbosswscxf;
+
+import net.jcip.annotations.Immutable;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.ws.addressing.WSAddressingFeature;
+import org.jboss.wise.core.wsextensions.DefaultEnablerDelegate;
+
+/**
+ * CXF version of EnablerDelegate 
+ * 
+ * @author alessio.soldano@jboss.com
+ */
+@Immutable
+public class CXFEnablerDelegate extends DefaultEnablerDelegate {
+
+    public CXFEnablerDelegate() {
+	super();
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.jboss.wise.core.wsextensions.EnablerDelegate#visitWSAddressing(Object)
+     */
+    public void visitWSAddressing(Object endpointInstance) throws UnsupportedOperationException {
+	Bus bus = BusFactory.getThreadDefaultBus();
+	Client client = ClientProxy.getClient(endpointInstance);
+	WSAddressingFeature feature = new WSAddressingFeature();
+	feature.initialize(client, bus);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.jboss.wise.core.wsextensions.EnablerDelegate#visitWSRM(Object)
+     */
+    public void visitWSRM(Object endpointInstance) throws UnsupportedOperationException {
+	throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void visitWSSecurity(Object endpointInstance) throws UnsupportedOperationException, IllegalStateException {
+	throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ integration-testsuite/cxf/src/test/java/org/jboss/wise/test/integration/wsaddressing/HelloImpl.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source Copyright 2006, JBoss Inc., and
+ * individual contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.jboss.wise.test.integration.wsaddressing;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.xml.ws.soap.Addressing;
+
+/**
+ * 
+ * @author alessio.soldano@jboss.com
+ * @since 20-Dic-2010
+ *
+ */
+
+@WebService( endpointInterface = "org.jboss.wise.test.integration.wsaddressing.Hello",
+             targetNamespace = "http://org.jboss/wise/wsa",
+             serviceName = "HelloService",
+             wsdlLocation = "WEB-INF/test.wsdl" ) //this is added to force the server on publishing a WSDL without policies; this way we're sure WS-Addressing
+             					  //on client side is controlled by features only, not by WS-Policy engine.
+@Stateless
+@Addressing(enabled = true, required = false)
+public class HelloImpl implements Hello {
+    
+    public String echoUserType( String user ) {
+        return "Hello WSAddressing";
+    }
+}
